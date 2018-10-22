04c1329a7ef31b0c1d0320e85e789942937a3abf
Jacek Maciupa <spaghetti147@gmail.com>
2018-10-22 11:05:12 +0200

    Initial commit

---
+++ src/main/java/pl/project/promanage/client/ClientController.java
@@ -0,0 +1,9 @@
+package pl.project.promanage.client;
+
+
+public class ClientController {
+
+
+
+
+}
---
+++ src/main/java/pl/project/promanage/client/ClientRepository.java
@@ -0,0 +1,7 @@
+package pl.project.promanage.client;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClientRepository extends CrudRepository<Client, Long> {
+
+}
